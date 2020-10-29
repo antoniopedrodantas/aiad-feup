cf8950435c64cee1615f85d303855f537f269591
vitorhugo13 <vhlg99@gmail.com>
2020-10-29 10:32:04 +0000

    RequestAgent created

---
+++ 1st Assignment/src/agents/RequestAgent.java
@@ -0,0 +1,19 @@
+package agents;
+
+import jade.core.Agent;
+
+@SuppressWarnings("serial")
+public class RequestAgent extends Agent{
+	
+	public RequestAgent() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void takeDown() {
+		
+	}
+}
