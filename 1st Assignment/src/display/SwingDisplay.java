4350958ee0c0119489f9530b660b63a1a0526384
António Dantas <44146077+antoniopedrodantas@users.noreply.github.com>
2020-11-04 12:58:03 +0000

    Added lift's info to display

--- 1st Assignment/src/display/SwingDisplay.java
+++ 1st Assignment/src/display/SwingDisplay.java
@@ -23,12 +23,14 @@ public class SwingDisplay implements Runnable {
 	
 	public SwingDisplay(ArrayList<LiftAgent> lifts, ArrayList<FloorPanelAgent> floorPanels) {
 		
+		// lifts and floor panels
 		this.lifts = lifts;
 		this.floorPanels = floorPanels;
 		
+		// initializes frame
 		this.frame = new JFrame("LiftManagementSystem");
         this.panel = new JPanel();
-        this.frame.setSize(500, 750);
+        this.frame.setSize(1250, 750);
 	}
 
 	@Override
@@ -47,13 +49,20 @@ public class SwingDisplay implements Runnable {
 		    this.panel.setLayout(null);
 		    this.panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		    
+		    // Draws lift info Header
+	    	label = new JLabel("Lift's Info:");
+	    	label.setPreferredSize(new Dimension(500, 20));
+	    	Dimension size = label.getPreferredSize();
+	    	label.setBounds(750, 50, size.width, size.height);
+	    	this.panel.add(label);
+		    
+		    // iterates through floor panels
 		    for(FloorPanelAgent fp: this.floorPanels) {
 		    	
 		    	// Draws FloorPanel
-		    	
 		    	label = new JLabel("FP" + fp.getFloor());
 		    	label.setPreferredSize(new Dimension(50, 20));
-		    	Dimension size = label.getPreferredSize();
+		    	size = label.getPreferredSize();
 		    	label.setBounds(50, 650 - (offsetY * fp.getFloor()), size.width, size.height);
 		    	this.panel.add(label);
 		    	
@@ -68,12 +77,27 @@ public class SwingDisplay implements Runnable {
 		    	
 		    }
 		    
+		    // this variable acts as the offset for the Lift's Info
+		    int i = 0;
+		    
+		    // iterates through lifts
 		    for(LiftAgent l : this.lifts) {
+		    	
+		    	// Draws Lift
 		    	label = new JLabel("Lift" + l.getId());
 		    	label.setPreferredSize(new Dimension(200, 20));
-		    	Dimension size = label.getPreferredSize();
+		    	size = label.getPreferredSize();
 		    	label.setBounds(50 + (offsetX * l.getId()), 650 - (offsetY * l.getFloor()), size.width, size.height);
 		    	this.panel.add(label);
+		    	
+		    	// Draws lift info
+		    	label = new JLabel("--> Lift" + l.getId() + ": CurrentFloor [" + l.getFloor() + "]; TaskList = {" + l.getTaskList() + "}; CurrentWeight: " + l.getCurrentWeight());
+		    	label.setPreferredSize(new Dimension(500, 20));
+		    	size = label.getPreferredSize();
+		    	label.setBounds(750, 75 + (25 * i), size.width, size.height);
+		    	this.panel.add(label);
+		    	
+		    	i++;
 		    }
 		    
 		    this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
