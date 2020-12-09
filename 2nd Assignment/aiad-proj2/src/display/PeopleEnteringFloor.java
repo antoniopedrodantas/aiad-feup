7821ebc0274b81cc68b63394827d8995010d7b4b
vitorhugo13 <vhlg99@gmail.com>
2020-12-09 12:59:33 +0000

    Missing to get info from analysis

--- 2nd Assignment/aiad-proj2/src/display/PeopleEnteringFloor.java
+++ 2nd Assignment/aiad-proj2/src/display/PeopleEnteringFloor.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import agents.FloorPanelAgent;
 import launcher.RepastLauncher;
+import uchicago.src.sim.analysis.OpenSequenceGraph;
+import uchicago.src.sim.engine.Schedule;
 import utils.Analysis;
 
 public class PeopleEnteringFloor {
@@ -11,10 +13,22 @@ public class PeopleEnteringFloor {
 	private ArrayList<FloorPanelAgent> floors;
 	private Analysis analysis;
 	private RepastLauncher repast;
+	private OpenSequenceGraph plot;
 	
 	public PeopleEnteringFloor(ArrayList<FloorPanelAgent> floors, Analysis analysis, RepastLauncher repast) {
 		this.floors = floors;
 		this.analysis = analysis;
 		this.repast = repast;
+		
+		if (this.plot != null) plot.dispose();
+			this.plot = new OpenSequenceGraph("People entering at floor", repast);
+		
+		this.plot.setAxisTitles("time", "People entering");
+		this.buildDisplay();
+		this.repast.getSchedule().scheduleActionAtInterval(2, plot, "step", Schedule.LAST);
+	}
+	
+	private void buildDisplay() {
+		
 	}
 }
--- 2nd Assignment/aiad-proj2/src/display/PeopleLeavingFloor.java
+++ 2nd Assignment/aiad-proj2/src/display/PeopleLeavingFloor.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 import agents.FloorPanelAgent;
 import launcher.RepastLauncher;
+import uchicago.src.sim.analysis.OpenSequenceGraph;
+import uchicago.src.sim.engine.Schedule;
 import utils.Analysis;
 
 public class PeopleLeavingFloor {
@@ -11,10 +13,22 @@ public class PeopleLeavingFloor {
 	private ArrayList<FloorPanelAgent> floors;
 	private Analysis analysis;
 	private RepastLauncher repast;
+	private OpenSequenceGraph plot;
 	
 	public PeopleLeavingFloor(ArrayList<FloorPanelAgent> floors, Analysis analysis, RepastLauncher repast) {
 		this.floors = floors;
 		this.analysis = analysis;
 		this.repast = repast;
+		
+		if (this.plot != null) plot.dispose();
+			this.plot = new OpenSequenceGraph("People exiting at floor", repast);
+			
+		this.plot.setAxisTitles("time", "People exiting");
+		this.buildDisplay();
+		this.repast.getSchedule().scheduleActionAtInterval(2, plot, "step", Schedule.LAST);
+	}
+	
+	private void buildDisplay() {
+		
 	}
 }
